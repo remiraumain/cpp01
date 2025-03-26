63654d849cc974d6b684595e03bfc6ae76f2b430
remiraumain <remiraumain@gmail.com>
2025-03-26 13:33:50 +0100

    Sed program done

---
+++ ex04/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 11:52:23 by rraumain          #+#    #+#             */
+/*   Updated: 2025/03/26 13:32:13 by rraumain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string replace(std::string line, const std::string &s1, const std::string &s2 )
+{
+	if (s1.empty())
+		return (line);
+
+	std::string res;
+	size_t pos = 0;
+	size_t found;
+
+	while ((found = line.find(s1, pos)) != std::string::npos)
+	{
+		res.append(line, pos, found - pos);
+		res.append(s2);
+		pos = found + s1.length();
+	}
+	res.append(line.substr(pos));
+	return (res);
+}
+
+int main( int argc, char ** argv )
+{
+	if (argc != 4)
+	{
+		std::cout << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
+		return (1);
+	}
+
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	std::ifstream infile(filename.c_str());
+	if (!infile)
+	{
+		std::cout << "Error: Could not open input file." << std::endl;
+		return (1);
+	}
+
+	std::ofstream outfile((filename + ".replace").c_str());
+	if (!outfile)
+	{
+		std::cout << "Error: Could not open output file." << std::endl;
+		return (1);
+	}
+
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		outfile << replace(line, s1, s2) << std::endl;
+	}
+	infile.close();
+	outfile.close();
+	return (0);
+}
