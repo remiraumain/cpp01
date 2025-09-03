b929139f72be507d6029e34f42f8afb1b0977f09
remiraumain <remiraumain@gmail.com>
2025-09-03 02:21:59 +0200

    ex04 now very ready for defense

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 11:52:23 by rraumain          #+#    #+#             */
-/*   Updated: 2025/09/03 01:46:01 by rraumain         ###   ########.fr       */
+/*   Updated: 2025/09/03 02:20:32 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int main( int argc, char **argv )
 {
 	if (argc != 4)
 	{
-		std::cout << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
+		std::cerr << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
 
@@ -45,17 +45,23 @@ int main( int argc, char **argv )
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 
+	if (s1.empty()) 
+	{
+        std::cerr << "Error: <s1> must not be empty." << std::endl;
+        return (1);
+    }
+
 	std::ifstream infile(filename.c_str());
 	if (!infile)
 	{
-		std::cout << "Error: Could not open input file." << std::endl;
+		std::cerr << "Error: Could not open input file." << std::endl;
 		return (1);
 	}
 
 	std::ofstream outfile((filename + ".replace").c_str());
 	if (!outfile)
 	{
-		std::cout << "Error: Could not open output file." << std::endl;
+		std::cerr << "Error: Could not open output file." << std::endl;
 		return (1);
 	}
 
@@ -63,8 +69,17 @@ int main( int argc, char **argv )
 	while (std::getline(infile, line))
 	{
 		outfile << replace(line, s1, s2) << std::endl;
+		if (!outfile)
+		{
+			std::cerr << "Error: write failed." << std::endl;
+			return (1);
+		}	
 	}
-	infile.close();
-	outfile.close();
+
+	if (infile.bad() || (infile.fail() && !infile.eof())) 
+	{
+		std::cerr << "Error: read failed." << std::endl; 
+		return (1); 
+	}
 	return (0);
 }
